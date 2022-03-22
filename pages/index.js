decd86f426b7ca1bf16b117434b8bf565ee31606
Dmitryzhur <toodual@gmail.com>
2022-03-22 22:39:27 +0500

    feat: добавить интерактивность сайта по 4 спринту

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,39 @@
-const popupElement = document.querySelector('.popup');
-const editButton = document.querySelector('.profile__edit-button');
-const closeButton = document.querySelector('.popup__close-button');
+let popup = document.querySelector('.popup');
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.popup__close-button');
+
+let textTitle = document.querySelector('.profile__title');
+let textAbout =  document.querySelector('.profile__subtitle');
+
+let formElement = document.querySelector('.popup__admin');
+let inputName = document.querySelector('#input__name');
+let inputAbout = document.querySelector('#input__about');
 
 
-function openPopup(popup) {
-  popupElement.classList.add('popup_opened');
+function openPopup() {
+  inputName.value = textTitle.textContent;
+  inputAbout.value = textAbout.textContent;
+  popup.classList.add('popup_opened');
 }
 
-function closePopup(popup) {
-  popupElement.classList.remove('popup_opened');
+function closePopup() {
+  popup.classList.remove('popup_opened');
 }
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                          // Так мы можем определить свою логику отправки.
+    textTitle.textContent = inputName.value;
+    textAbout.textContent = inputAbout.value;
+    closePopup();
+}
+
 
 editButton.addEventListener ('click', openPopup);
 
 closeButton.addEventListener ('click', closePopup);
+
+// Прикрепляем обработчик к форме:
+formElement.addEventListener('submit', formSubmitHandler);
