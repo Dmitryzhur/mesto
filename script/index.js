f479119759a581bc5249fdef2348d1273f925442
Dmitry Zhuravlev <dmitryzhur@mail.ru>
2022-04-05 21:24:26 +0500

    feat: добавить базовые карточки через js

--- script/index.js
+++ script/index.js
@@ -1,13 +1,65 @@
-let popup = document.querySelector('.popup');
-let editButton = document.querySelector('.profile__edit-button');
-let closeButton = document.querySelector('.popup__close-button');
+const popup = document.querySelector('.popup');
+const editButton = document.querySelector('.profile__edit-button');
+const closeButton = document.querySelector('.popup__close-button');
 
-let textTitle = document.querySelector('.profile__title');
-let textAbout =  document.querySelector('.profile__subtitle');
+const textTitle = document.querySelector('.profile__title');
+const textAbout =  document.querySelector('.profile__subtitle');
 
-let formElement = document.querySelector('.popup__admin');
-let inputName = document.querySelector('#input-name');
-let inputAbout = document.querySelector('#input-about');
+const formElement = document.querySelector('.popup__admin');
+const inputName = document.querySelector('#input-name');
+const inputAbout = document.querySelector('#input-about');
+
+const cardTemplate = document.querySelector('#elements__element-template').content;
+const cardsContainer = document.querySelector('.elements');
+
+const initialCards = [
+  {
+    name: 'Ночной Тагил',
+    link: './images/photo-street.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Дом на Урале',
+    link: './images/photo-house.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+// Создание карточки
+function createCard(elem) {
+  const newCardElement = cardTemplate.querySelector('.elements__element').cloneNode(true);
+  newCardElement.querySelector('.elements__element-img').src = elem.link;
+  newCardElement.querySelector('.elements__element-img').alt = elem.name;
+  newCardElement.querySelector('.elements__element-title').textContent = elem.name;
+  newCardElement.querySelector('.elements__element-trash-button');
+  newCardElement.querySelector('.elements__element-like-button');
+  return newCardElement;
+}
+
+// Добавление карточки на страницу
+function addNewCard(elem) {
+  const newCard = createCard(elem); // Добавить созданную карточку
+  cardsContainer.prepend(newCard); // Поставить карту вначале контейнера
+}
+
+// Выгрузка карточек из массива
+initialCards.forEach( elem => {
+  addNewCard(elem);
+})
 
 
 function openPopup() {
@@ -35,5 +87,5 @@ editButton.addEventListener ('click', openPopup);
 
 closeButton.addEventListener ('click', closePopup);
 
-// Прикрепляем обработчик к форме:
+// Прикрепляем обработчик к форме
 formElement.addEventListener('submit', formSubmitHandler);
