e5e5413b6fe49a172d18cdbf7b02b8085df99a72
Dmitryzhur <toodual@gmail.com>
2022-04-21 23:59:27 +0500

    feature: добавить валидацию двух форм. создать формы ошибок

--- script/validate.js
+++ script/validate.js
@@ -1,11 +1,84 @@
+// Функция показа ошибки валидации
+const showInputError = (formElement, inputElement, errorMessage, obj) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add(`${obj.inputErrorClass}`);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(`${obj.errorClass}`);
+};
+
+const hideInputError = (formElement, inputElement, obj) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove(`${obj.inputErrorClass}`);
+  errorElement.classList.remove(`${obj.errorClass}`);
+  errorElement.textContent = '';
+};
+
+// Проверка на валидность полей ввода
+const checkInputValidity = (formElement, inputElement, obj) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage, obj);
+  } else {
+    hideInputError(formElement, inputElement, obj);
+  }
+};
+
+const setEventListeners = (formElement, obj) => {
+  const inputList = Array.from(formElement.querySelectorAll(`${obj.inputElement}`));
+  const buttonElement = formElement.querySelector(`${obj.submitButtonSelector}`);
+  toggleButtonState(inputList, buttonElement, obj);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement, obj);
+      toggleButtonState(inputList, buttonElement, obj);
+    });
+  });
+};
+
+// Функция принимает массив полей
+const hasInvalidInput = (inputList) => {
+  // проходим по этому массиву методом some
+  return inputList.some((inputElement) => {
+    // Если поле не валидно, колбэк вернёт true
+    // Обход массива прекратится и вся функция
+    // hasInvalidInput вернёт true
+    return !inputElement.validity.valid;
+  })
+};
+
+// Функция принимает массив полей ввода
+// и элемент кнопки, состояние которой нужно менять
+const toggleButtonState = (inputList, buttonElement, obj) => {
+  // Если есть хотя бы один невалидный инпут
+  if (hasInvalidInput(inputList)) {
+    // сделай кнопку неактивной
+    buttonElement.classList.add(`${obj.inactiveButtonClass}`);
+    buttonElement.disabled = true;
+  } else {
+    // иначе сделай кнопку активной
+    buttonElement.classList.remove(`${obj.inactiveButtonClass}`);
+    buttonElement.disabled = false;
+  }
+};
+
+
+const enableValidation = (obj) => {
+  const formList = Array.from(document.querySelectorAll(`${obj.formSelector}`));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement, obj);
+  });
+};
+
 // включение валидации вызовом enableValidation
 // все настройки передаются при вызове
 
 enableValidation({
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
+  formSelector: '.popup__admin',
+  inputElement: '.popup__item',
   submitButtonSelector: '.popup__button',
   inactiveButtonClass: 'popup__button_disabled',
-  inputErrorClass: 'popup__input_type_error',
+  inputErrorClass: 'popup__item_type_error',
   errorClass: 'popup__error_visible'
 });
